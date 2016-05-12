300bc3f680d4f4c2448342397dbfc7634b178acf
Rok Vrckovnik <vrckovnik.rok@gmail.com>
2016-05-12 14:11:33 +0200

    Spremembe v podatkih

--- podatki/Podatki.R
+++ podatki/Podatki.R
@@ -1,4 +1,4 @@
-setwd("U:/OPB/backup")
+setwd("U:/Profit/podatki")
 Energy<-read.csv("Energy1.csv", sep=";")
 Technology<-read.csv("Technology1.csv", sep=";")
 Finance<-read.csv("Finance1.csv", sep=";")
@@ -9,3 +9,6 @@ Finance_symbol<-Finance$Symbol[c(121,227,493,532,993)]
 Energy5<-Energy[c(52,115,123,223,286),c(-5,-9)]
 Finance5<-Finance[c(121,227,493,532,993),c(-5,-9)]
 Technology5<-Technology[c(33,34,48,211,365),c(-5,-9)]
+
+
+
