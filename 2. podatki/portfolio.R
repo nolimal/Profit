7553e5d8e0ad9ef7ec6326e455f36b5e0ef5c955
VidStarc <vid.starc9@gmail.com>
2016-06-01 20:28:06 +0200

    popravek

--- 2. podatki/portfolio.R
+++ 2. podatki/portfolio.R
@@ -1,8 +1,16 @@
-portfolio<-data.frame(row.names(WFC))
-names(portfolio)<-c("date")
+datum<-data.frame(row.names(WFC))
+names(datum)<-c("date")
+w<-c()
+for (i in (2:nrow(datum))){
+  w<-c(w,rep(datum[i,1],3))
+}
+date<-w
+
+
+#stoplec selected tickers
 n<-length(all.tickers$ticker)
 nn<-nrow(stockData$WFC)
-
+sez<-c()
 m<-c()
 for (i in 1:(nn-1)){
   a<-seq(1,n,nn)+i
@@ -17,7 +25,13 @@ for (i in 1:(nn-1)){
       m<-c(m,all.tickers[[k,1]])
     }
   }
+  sez<-c(sez,length(m))
 }
+sel.tickers<-m
+#problem, na en dan(na 182 dan) smo zbrali 4 delnice!!
+sez[180:190]
+
+#portfolio<-data.frame(date,sel.tickers)
 
 
 
