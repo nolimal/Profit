a6d05316a922ac4f8e172a58781798cb769743a4
VidStarc <vid.starc9@gmail.com>
2016-06-01 19:49:28 +0200

    zanka za izbrane tickerje v portfolio

--- 2. podatki/portfolio.R
+++ 2. podatki/portfolio.R
@@ -3,18 +3,23 @@ names(portfolio)<-c("date")
 n<-length(all.tickers$ticker)
 nn<-nrow(stockData$WFC)
 
-vektor<-seq(1,n,nn)
+m<-c()
 for (i in 1:(nn-1)){
   a<-seq(1,n,nn)+i
-  vektor<-c(vektor,a)
   price<-c()
   for (j in a){
     b<-all.tickers[[j,9]]
     price<-c(price,b)
   }
   dy<-sort(price,decreasing = TRUE)[1:3]
-#   majstore<-c()
-#   for (k in a){
-#     ifelse(all.tickers[[k,9]]%in%dy, majstore<-c(majstore,all.tickers[[k,2]]),break)
-#   }
+  for (k in a){
+    if(all.tickers[[k,9]]%in%dy){
+      m<-c(m,all.tickers[[k,1]])
+    }
+  }
 }
+
+
+
+
+
