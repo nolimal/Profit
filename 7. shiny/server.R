22f8a9b624f30538c18a073390aa8ca29e217606
Matevz Nolimal <matevz.nolimal@live.com>
2016-06-08 18:23:05 +0200

    shiny 7 ok

--- 7. shiny/server.R
+++ 7. shiny/server.R
@@ -19,16 +19,13 @@ shinyServer(function(input, output) {
   # Fill in the spot we created for a plot
   output$portfolio <- renderPlot({
     tab <- tbl.portfolio
-    data <- tab %>% filter(index == indeks.portfelja, date2 == datum) %>%
-      select(sel_tickers, am) %>% data.frame()
-    if (input$type != "All") {break
-    } else {data <- tab %>% filter(index == input$index,
-                                   date2 >= input$daterange[1],
-                                   date2 <= input$daterange[2]) %>%
-      select(date2, sel_tickers, am) %>% data.frame()
+    data <- tab %>% filter(index == input$index,
+                           date2 >= input$daterange[1],
+                           date2 <= input$daterange[2]) %>%
+      group_by(date2) %>% summarise(am = sum(am)) %>% data.frame()
     data$date2 <- as.Date(data$date2)
-    graf<-ggplot(data, aes(x = date2, y = am, color = sel_tickers)) +
-      geom_line() + xlab("Date") + ylab("Asset Movement")}
+    graf<-ggplot(data, aes(x = date2, y = am)) +
+      geom_line() + xlab("Date") + ylab("Asset Movement")
     graf
   })
   
