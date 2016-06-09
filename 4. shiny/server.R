ae59e47caad364aeec3880bc47b8b6a6a804891f
Matevz Nolimal <matevz.nolimal@live.com>
2016-06-09 14:06:26 +0200

    Final version

--- 4. shiny/server.R
+++ 4. shiny/server.R
@@ -13,6 +13,11 @@ shinyServer(function(input, output) {
   conn <- src_postgres(dbname = db, host = host,
                        user = user, password = password)
   tbl.company <- tbl(conn, "company")
+  tbl.stock <- tbl(conn, "stock")
+  tbl.stock1 <- tbl(conn, "stock")
+  tbl.portfolio <- tbl(conn, "portfolio")
+  
+######################################################
   
   # Fill in the spot we created for a plot
   output$company <- renderPlot({
@@ -24,6 +29,77 @@ shinyServer(function(input, output) {
     ggplot(data.frame(tab), aes_string(x = "ticker", y = "market_cap")) + geom_bar(stat = "identity") +
       ggtitle(input$type) + xlab("Ticker") + ylab("Market cap")
   })
+  
+######################################################
+  
+  output$stock <- renderPlot({
+    validate(need(length(input$ticker) > 0, "Izberi vsaj en ticker!"))
+    tab <- tbl.stock
+    if (length(input$ticker) == 1) {
+      tab <- tab %>% filter(ticker == input$ticker)
+    } else {
+      tab <- tab %>% filter(ticker %in% input$ticker)
+    }
+    
+    ggplot(data.frame(tab), aes_string(x = "date", y = input$value, color="ticker")) + geom_bar(stat = "identity") +
+      ggtitle(input$ticker) + xlab("Date") + ylab(input$value) + 
+      geom_point(aes_string(x = "date", y = input$value), colour='blue', size=0.5)
+  })
+  
+######################################################
+  
+  output$stock1 <- renderPlot({
+    tab <- tbl.stock1
+    if (input$ticker1 == "All") { # prikazujemo vse delnice
+      max.index <- tab %>% summarise(max(index)) %>% data.frame() %>% .[,1] %>%
+        strftime() # pridobimo čas najnovejših vrednosti in samo te prikažemo
+      graf <- ggplot(tab %>% filter(index == max.index) %>% data.frame(),
+                     aes_string(x = "ticker", y = input$value1, color="ticker")) +
+        geom_bar(stat = "identity")
+    } else { # prikazujemo posamezno delnico
+      graf <- ggplot(tab %>% filter(ticker == input$ticker1) %>% data.frame(),
+                     aes_string(x = "index", y = input$value1, color="ticker")) + geom_line()
+    }
+    graf + ggtitle(input$ticker1) + xlab("Date") + ylab(input$value1)
+  })
+  
+######################################################
+  
+  portfolio.data <- reactive({
+    data <- tbl.portfolio
+    if (input$index != "All") {
+      data <- data %>% filter(index == input$index)
+    }
+    data <- data %>% filter(date2 >= input$daterange[1],
+                            date2 <= input$daterange[2]) %>%
+      group_by(index, date2)
+    if (input$index == "All") {
+      data <- data %>% summarise(am = sum(am))
+    } else {
+      data <- data %>% summarise(am = sum(am),
+                                 tickers = string_agg(sel_tickers, ","))%>%
+        arrange(date2)
+    }
+    data <- data %>% data.frame()
+    data$date2 <- as.Date(data$date2)
+    data
+  })
+  
+  output$portfolio <- renderPlot({
+    data <- portfolio.data()
+    graf <- ggplot(data, aes(x = date2, y = am, color = index, group = index)) +
+      geom_line() + xlab("Date") + ylab("Asset Movement")
+    graf
+  })
+  
+  output$tickers <- renderTable({
+    validate(need(input$index != "All", ""))
+    data <- portfolio.data()
+    tickers <- data$tickers %>% strsplit(",") %>% as.data.frame() %>% t()
+    rownames(tickers) <- NULL
+    df <- data.frame(as.character(data$date2), tickers)
+    names(df) <- c("Datum", paste("Ticker", 1:input$index))
+    df
+  })
+  
 })
-
-
--- 4. shiny/ui.R
+++ 4. shiny/ui.R
@@ -1,54 +1,134 @@
 library(shiny)
 
-# shinyUI(fluidPage(
-#   
-#   titlePanel("Company"),
-#   
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("min",
-#                   "Minimalni znesek transakcije:",
-#                   min = -10000,
-#                   max = 10000,
-#                   value = 1000)
-#     ),
-#     
-#     mainPanel(
-#       tableOutput("transakcije")
-#     )
-#   )
-# ))
-
-
-# Izbira
-library(datasets)
-
 # Define the overall UI
 shinyUI(
-  
-  # Use a fluid Bootstrap layout
-  fluidPage(    
+  fluidPage(
     
-    # Give the page a title
-    titlePanel("Companies by type"),
+    titlePanel("Profit"),
     
-    # Generate a row with a sidebar
-    sidebarLayout(      
+    tabsetPanel(
       
-      # Define the sidebar with one input
-      sidebarPanel(
-        selectInput("type", "Type:", 
-                    choices=c("All", "Energy","Finance","Technology")),
-        hr(),
-        helpText("Izbira tipa.")
-      ),
+######################################################
       
-      # Create a spot for the barplot
-      mainPanel(
-        plotOutput("company")  
-      )
+      tabPanel("Companies by type",
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   selectInput("type", "Type:", 
+                               choices=c("All", "Energy","Finance","Technology")),
+                   hr(),
+                   helpText("Izbira tipa.")
+                 ),
+                 
+                 # Create a spot for the barplot
+                 mainPanel(
+                   plotOutput("company")  
+                 )
+                 
+               )
+               ),
+
+######################################################
+      
+      tabPanel("Companies by ticker",
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   # wellPanel(
+                   #   p(strong("stock")),
+                   #   checkboxInput(inputId = "AAPL", label = "Apple (AAPL)",     value = TRUE)
+                   # ),
+                   # selectInput(inputId="ticker",
+                   #             label="Ticker",
+                   #             choices = c("Candlestick" = "candlesticks",
+                   #                         "Matchstick" = "matchsticks",
+                   #                         "Bar" = "bars",
+                   #                         "Line" = "line")
+                   #             ),
+                   selectInput("ticker", "Ticker:",
+                               choices=c("TOT","PTR","CVX","GE","XOM","FB","MSFT","GOOG",
+                                         "GOOGL","AAPL","HSBC","C","BAC","JPM","WFC"),
+                               selected = "TOT", multiple = TRUE
+                   ),
+                   selectInput("value", "Vrednost", 
+                               choices=c("open", "high", "low",
+                                         "close", "volume", "adjusted","change"),
+                               selected = "open"),
+                   hr(),
+                   helpText("Izbira tickerja")
+                 ),
+                 
+                 # Create a spot for the barplot
+                 mainPanel(
+                   plotOutput("stock")
+                   # plotOutput("stock_open")
+                   # plotOutput("stock_close"),
+                   # plotOutput("stock_volume")
+                 )
+                 
+               )
+               ),
+
+######################################################
+      
+      tabPanel("Stock Data",
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   wellPanel(
+                     p(strong("Stocks"))
+                   ),
+                   selectInput("ticker1", "Ticker", 
+                               choices=c("All","WFC","JPM","BAC","C","HSBC","AAPL","GOOGL",
+                                         "GOOG","MSFT","FB","XOM","GE","CVX","PTR","TOT")),
+                   selectInput("value1", "Vrednost", 
+                               choices=c("open", "high", "low",
+                                         "close", "volume", "adjusted","change"),
+                               selected = "close"),
+                   hr(),
+                   helpText("Izbira delnice in vrednosti.")
+                 ),
+                 
+                 # Create a spot for the barplot
+                 mainPanel(
+                   plotOutput("stock1")  
+                 )
+                 
+               )
+               ),
+
+######################################################
+      
+      tabPanel("Portfolio Analysis",
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   wellPanel(
+                     p(strong("Portfolio"))
+                   ),
+                   dateRangeInput(inputId = "daterange", label = "Date range",
+                                  start = "2016-01-05", end = Sys.Date()),
+                   selectInput("index", "Index", 
+                               choices=c("All", "1", "3", "7")
+                   ),
+                   hr(),
+                   helpText("Izbira datuma in portfolia.")
+                 ),
+                 
+                 # Create a spot for the barplot
+                 mainPanel(
+                   plotOutput("portfolio"),
+                   tableOutput("tickers")
+                 )
+                 
+               )
+               )
       
     )
+    
   )
 )
-
